4ae63a401d8ef0f05a8ed13b7b45664723d1cfcd
ZedMaks <vodafone.shop245@gmail.com>
2022-02-10 15:36:26 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+
+let userName = prompt("Як тебе звати друже?");
+let userHome = prompt("Де ти живеш?");
+alert(`Привіт,${userName},ти живеш ${userHome} вірно?`);
+console.log(`Привіт,${userName},я знаю, що ти живеш ${userHome}))`);
+document.write(`<div>Привіт,${userName},я знаю, що ти крутий!</div>`);
